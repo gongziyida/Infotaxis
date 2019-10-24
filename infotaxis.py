Fixed wind direction calculation error

infotaxis.py
@@ -60,7 +60,8 @@ class Infotaxis:
         else:
             raise ValueError('Unsupported dimension')
 
-        self._v = np.array([np.cos(winddir), np.sin(winddir)]) * v  # Vectorize
+        self._v = np.array([-np.sin(winddir), np.cos(winddir)]) * v  # Vectorize
+        self._v[self._v < 1e-5] = 0
 
         self._pos = self.xy2ij(pos)
 
@@ -137,6 +138,10 @@ class Infotaxis:
     @property
     def prob_map(self):
         return self._prob_map
+
+    @property
+    def wind(self):
+        return (self._v[1], -self._v[0])
 
     def concentration_map(self):
         """

infotaxis_gui.py
@@ -57,7 +57,7 @@ class InfotaxisRealTime(QtGui.QWidget):
         self.hist_pos = self.searcher_pos.copy()
         self.hits_record = [False]
         dim = self.infotaxis._dim
-        v = self.infotaxis._v
+        v = self.infotaxis.wind
         tau = float(self.infotaxis._tau)
         d = float(self.infotaxis._d)
         r = float(self.infotaxis._r)
@@ -147,7 +147,7 @@ if __name__ == '__main__':
     # Create an infotaxis instance
     infotaxis_example = itx.Infotaxis(
         d=2, r=100, a=1, v=0.5, winddir=3 * np.pi / 2, tau=500, dt=0.1, dim=3,
-        pos=np.zeros(2), src_pos=np.full(2, 90), src_radius=2, window=100)
+        pos=np.array([0., 0.]), src_pos=np.full(2, 90), src_radius=2, window=100)
 
     # Make a GUI
     infotaxis_real_time = InfotaxisRealTime(infotaxis_example)